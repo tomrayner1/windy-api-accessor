5fb60a670c31827e94d39acf61ff82b53981e090
Tom Rayner <52078213+tomrayner1@users.noreply.github.com>
2025-03-22 11:11:03 +0000

    Update runner

--- tests/sql_test.py
+++ tests/sql_test.py
@@ -1,16 +1,26 @@
 import mysql.connector
 
+# mysql://root:qzRTZodSVLiKMWATQJWLWOgcThBxqaem@hopper.proxy.rlwy.net:43283/railway
 
 def sql_test():
   conn = mysql.connector.connect(
     host="hopper.proxy.rlwy.net",
     user="root",
-    password="zRTZodSVLiKMWATQJWLWOgcThBxqaem",
+    password="qzRTZodSVLiKMWATQJWLWOgcThBxqaem",
     database="NahrimWaterDashboard",
     port=43283
   )
 
   cursor = conn.cursor()
+
+  sql = """
+SELECT * FROM Temperature_Data;
+"""
+
+  cursor.execute()
+
+  for row in cursor:
+    print(f"{row}")
 
   cursor.close()
   conn.close()
